a47909b876bddab38b0a29dc6eee909dd75e9aac
luca-sk <69893735+luca-sk@users.noreply.github.com>
2021-01-19 11:34:21 +0100

    Add files via upload

---
+++ Luca/SnakeGame_V3/SnakeGame/MyWorld.java
@@ -0,0 +1,40 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    Player bluePlayer = new Player(0, 0,0,255);
+    Player greenPlayer = new Player (4,0,255,0);
+    Counter blueCounter = new Counter();
+    Counter greenCounter = new Counter();
+    int count = 0;
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1); 
+        getBackground().setColor(Color.BLACK);
+        getBackground().fill();
+        addObject(bluePlayer, 150, 550);
+        addObject(blueCounter, 50, 25);
+        addObject(greenPlayer, 650, 550); 
+        addObject(greenCounter, 750, 25);
+    }
+
+    public void act()
+    {
+        count++;
+        if(count >100){
+            addObject(new Food(255, 0 , 0), Greenfoot.getRandomNumber(getWidth() -1), Greenfoot.getRandomNumber(getHeight() -1));
+            count = 0;}
+    }
+}
